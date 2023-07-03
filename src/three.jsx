121c6e47b1b44ab3cc707b823592196f38d2ce0f
Danil <123034340+soorq@users.noreply.github.com>
2023-07-03 21:59:47 +0300

    Add files via upload
    Upload - Main

---
+++ src/three.jsx
@@ -0,0 +1,14 @@
+// const Boxes = ({ position = [] }) => {
+// 	const ref = useRef(null)
+
+// 	useFrame(() => (ref.current.rotation.x += 0.01), [])
+
+// 	return (
+// 		<>
+// 			<mesh position={position} ref={ref}>
+// 				<boxGeometry drawRange={23} />
+// 				<meshBasicMaterial color='red' />
+// 			</mesh>
+// 		</>
+// 	)
+// }
